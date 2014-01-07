236b26d6fdf796ab817df6a670e8b42900857837
Ronijf <roni.jf2@gmail.com>
2014-01-07 15:33:23 +0700

    script

--- login.sh
+++ login.sh
@@ -8,9 +8,9 @@ echo "---";
 for PID in "${data[@]}"
 do
 	#echo "check $PID";
-	NUM=`cat /var/log/secure | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | wc -l`;
-	USER=`cat /var/log/secure | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | awk '{print $10}'`;
-	IP=`cat /var/log/secure | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | awk '{print $12}'`;
+	NUM=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | wc -l`;
+	USER=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | awk '{print $10}'`;
+	IP=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | awk '{print $12}'`;
 	if [ $NUM -eq 1 ]; then
 		echo "$PID - $USER - $IP";
 	fi
